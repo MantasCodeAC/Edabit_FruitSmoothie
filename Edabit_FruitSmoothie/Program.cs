448afcf81945f675f17ca01f1509069d93b8150d
Mantas Razanta <mantas.razanta@codeacademylt.onmicrosoft.com>
2022-09-21 21:40:11 +0300

    Add project files.

---
+++ Edabit_FruitSmoothie/Program.cs
@@ -0,0 +1,23 @@
+﻿using Edabit_FruitSmoothie;
+/*Create a class Smoothie and do the following:
+Create a property called Ingredients.
+Create a GetCost method which calculates the total cost of the ingredients used to make the smoothie.
+Create a GetPrice method which returns the number from GetCost plus the number from GetCost multiplied by 1.5. 
+Round to two decimal places.
+Create a GetName method which gets the ingredients and puts them in alphabetical order into a nice descriptive sentence. 
+If there are multiple ingredients, add the word "Fusion" to the end but otherwise, add "Smoothie".
+Ingredient	Price
+Strawberries	£1.50
+Banana	£0.50
+Mango	£2.50
+Blueberries	£1.00
+Raspberries	£1.00
+Apple	£1.75
+Pineapple	£3.50
+ */
+
+List<string> sarasas1 = new List<string>() { "Strawberries", "Banana", "Blueberries", "Raspberries", "Apple", "Raspberries" };
+Smoothie kokteilis1 = new Smoothie(sarasas1);
+Console.WriteLine($"Kokteilių savikaina lygi {kokteilis1.GetCost()}eur.");
+Console.WriteLine($"Kokteilių pardavimo kaina yra {kokteilis1.GetPrice(kokteilis1.GetCost())}eur");
+kokteilis1.GetName(sarasas1);
---
+++ Edabit_FruitSmoothie/Smoothie.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit_FruitSmoothie
+{
+    internal class Smoothie
+    {
+        public List<string> Ingredients;
+
+        public Smoothie(List<string> ingredients)
+        {
+            Ingredients = ingredients;
+        }
+
+        public double GetCost()
+        {
+            double getcost = 0;
+            foreach (var i in Ingredients)
+            {
+                switch (i)
+                {
+                    case "Strawberries":
+                        getcost = getcost + 1.5;
+                        break;
+                    case "Banana":
+                        getcost = getcost + 0.5;
+                        break;
+                    case "Mango":
+                        getcost = getcost + 2.5;
+                        break;
+                    case "Blueberries":
+                        getcost = getcost + 1.0;
+                        break;
+                    case "Raspberries":
+                        getcost = getcost + 1.0;
+                        break;
+                    case "Apple":
+                        getcost = getcost + 1.75;
+                        break;
+                    case "Pineapple":
+                        getcost = getcost + 3.50;
+                        break;    
+                }
+            }
+            return getcost;
+        }
+
+        public decimal GetPrice(double getcost)
+        {
+            decimal getPrice= Convert.ToDecimal(getcost + (getcost * 1.5));
+            getPrice = Math.Round(getPrice, 2);
+            return getPrice;
+        }
+
+        public void GetName(List<string> Ingredients)
+        {
+            Ingredients.Sort();
+            if (Ingredients.Count != Ingredients.Distinct().Count())
+            {
+                Ingredients.Add("Fusion");
+            }
+            else
+            {
+                Ingredients.Add("Smoothie");
+            }
+            Console.WriteLine("Kokteilio pavadinimas: ");
+            foreach (var j in Ingredients)
+            {
+                Console.Write(j + " ");
+            }
+        }
+    }
+}
